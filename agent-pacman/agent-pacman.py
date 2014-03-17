b2715278b0feafc5dfa0e1a231c830eb85c79b5f
Adrian Ortega <elfus0.1@gmail.com>
2014-03-16 19:54:00 -0600

    Added support to move the character with a reasonable speed.

--- agent-pacman/agent-pacman.py
+++ agent-pacman/agent-pacman.py
@@ -1,11 +1,30 @@
 #! /usr/bin/python
 __author__ = 'aortegag'
 
-import sys, pygame
+import pygame
+import pygame.constants
+from pygame.locals import *
 
-OFFSET = 1
+OFFSET = 3
 X_AXIS = 0
 Y_AXIS = 1
+
+def handle_event(event):
+    """
+    Handles a pygame.K_DOWN event
+    :param event: A pygame.event
+    :return: The direction in whic pacman has to move
+    """
+    direction = [0, 0]
+    if event.key == pygame.K_w:
+        direction = [0, -OFFSET]
+    elif event.key == pygame.K_s:
+        direction = [0, OFFSET]
+    elif event.key == pygame.K_a:
+        direction = [-OFFSET, 0]
+    elif event.key == pygame.K_d:
+        direction = [OFFSET, 0]
+    return direction
 
 def handle_input():
     """
@@ -16,11 +35,11 @@ def handle_input():
     direction = [0, 0]
     if pygame.key.get_pressed()[pygame.K_w]:
         direction = [0, -OFFSET]
-    if pygame.key.get_pressed()[pygame.K_s]:
+    elif pygame.key.get_pressed()[pygame.K_s]:
         direction = [0, OFFSET]
-    if pygame.key.get_pressed()[pygame.K_a]:
+    elif pygame.key.get_pressed()[pygame.K_a]:
         direction = [-OFFSET, 0]
-    if pygame.key.get_pressed()[pygame.K_d]:
+    elif pygame.key.get_pressed()[pygame.K_d]:
         direction = [OFFSET, 0]
 
     return direction
@@ -28,10 +47,10 @@ def handle_input():
 def main():
     pygame.init()
 
-    ball = pygame.image.load("res/ball.gif")
+    ball = pygame.image.load("res/rojo.png")
     ballrect = ball.get_rect()
 
-    pacman_background = pygame.image.load("res/pac-man-background.jpg")
+    pacman_background = pygame.image.load("res/tableropacman.jpg")
     pacman_rect = pacman_background.get_rect()
     window_size = width, height = pacman_background.get_width(), pacman_background.get_height()
 
@@ -41,22 +60,29 @@ def main():
     # displayed graphics. Any drawing you do to this Surface will become
     # visible on the monitor.
     screen = pygame.display.set_mode(window_size)
+    pygame.display.set_caption("Agent Pacman")
 
     # Convert the pixel formats to that of the same type as our screen.
     ball = ball.convert_alpha() # Convert_alpha keeps the alpha channel in the pixel
-    pacman_background = pacman_background.convert() # Removes alpha channel
+    pacman_background = pacman_background.convert_alpha() # Removes alpha channel
 
     screen.blit(pacman_background,pacman_rect)
+    pygame.key.set_repeat(50,50)
     while 1:
+        direction = [0,0]
         # Detect the input and get a new direction
         # NOTE: This is the line that will be replaced by the agents once they are implemented
-        direction = handle_input()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            if event.type == pygame.KEYDOWN:
+                direction = handle_event(event)
 
-        # Fill the background that was erased by the ball
+         # Fill the background that was erased by the ball
         screen.blit(pacman_background,ballrect,ballrect)
 
         # The class pygame.Rect is a rectanble represeting the position of the ball
-        ballrect = ballrect.move(direction)
+        ballrect.move_ip(direction)
 
         # If we are out of boundaries stay inside
         if ballrect.left < 0 or ballrect.right > width:
@@ -79,10 +105,6 @@ def main():
         # since we were drawing the back buffer it's time to flip it
         # and make it available on the front buffer
         pygame.display.flip()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
 
 if __name__ == "__main__":
     main()
