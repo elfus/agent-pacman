2903aa30ee9e55e59c445b4d51a888b2202dfe5e
Adrian Ortega Garcia <adrian.ortega.garcia@intel.com>
2014-04-29 14:55:35 -0500

    Added methods to start calculating pacman's behavior

--- agent-pacman/pacman_ai.py
+++ agent-pacman/pacman_ai.py
@@ -1,6 +1,7 @@
 __author__ = 'aortegag'
 
 from characters import *
+import pygame
 
 class WorldState:
     def __init__(self, pointsGroup):
@@ -28,15 +29,48 @@ def getPossibleActions():
         possible_actions.append(GO_UP)
     return possible_actions
 
-def agent_policy(state, action):
+def get_closest_pacman_point(state):
+    h_list = []
+    for point in state.dots:
+        h = Character.PACMAN.pitagorazo(state.pacman_xy[0]-point.rect.centerx,
+                                             state.pacman_xy[1]-point.rect.centery)
+        if state.pacman_tile.rect.center != point.rect.center:
+            h_list.append((h,point))
+
+    mtuple = min(h_list, key=lambda item:item[0])
+    return mtuple[1]
+
+def get_tile_from_pacman_point(ppoint):
+    board = Character.PACMAN.board_matrix
+    i  = 0
+    for row in board:
+        for tile in row:
+            if ppoint.rect.center == tile.rect.center:
+                return tile
+    return 0
+
+def reward_function(state, action, number_actions):
+    """
+    The reward function tells pacman what is good in an immediate sense. It tells pacman
+    the desirability to apply the given action in the current state
+
+    """
+    ppoint = get_closest_pacman_point(state)
+    ppoint_tile = get_tile_from_pacman_point(ppoint)
+    desired_direction = Character.PACMAN.get_direction_from_to(state.pacman_tile,ppoint_tile)
+    pr = 1.0/number_actions
+    return pr
+
+
+def agent_policy(state, action, number_actions):
     """
     Calculates the probability that the action will be performed in the current state
     :param state:
     :param action:
     :return:
     """
-
-    return 1
+    val = reward_function(state, action, number_actions)
+    return val
 
 def reinforcement_learning_get_direction(pointsGroup):
     """
@@ -54,8 +88,9 @@ def reinforcement_learning_get_direction(pointsGroup):
     actions = getPossibleActions()
     pr_list = []
     for action in actions:
-        pr = agent_policy(mState, action)
-        pr_list.append((pr, action))
+        pr = agent_policy(mState, action, len(actions))
+        if pr != 0:
+            pr_list.append((pr, action))
 
-    max_tuple = max(pr_list)
+    max_tuple = max(pr_list, key=lambda item:item[0])
     return max_tuple[1]
